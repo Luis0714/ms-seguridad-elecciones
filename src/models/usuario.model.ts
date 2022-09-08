bbec78d1756cd6d6e8d0d338a5f594099d0036d2
Luis Andres Martinez Estremor <luian.me0714@gmail.com>
2022-09-08 11:56:29 -0500

    Creacion de proyecto de seguridad con Modelos,DataSource,Controladores para Usuario,coneccion a base de datos MongoDB

---
+++ src/models/usuario.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property, belongsTo, hasMany} from '@loopback/repository';
+import {Rol} from './rol.model';
+import {AuditoriaLogin} from './auditoria-login.model';
+
+@model({settings: {strict: false}})
+export class Usuario extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombres: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Telefono: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Correo: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Clave: string;
+
+  @belongsTo(() => Rol)
+  rolId: string;
+
+  @hasMany(() => AuditoriaLogin)
+  auditorias: AuditoriaLogin[];
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Usuario>) {
+    super(data);
+  }
+}
+
+export interface UsuarioRelations {
+  // describe navigational properties here
+}
+
+export type UsuarioWithRelations = Usuario & UsuarioRelations;
